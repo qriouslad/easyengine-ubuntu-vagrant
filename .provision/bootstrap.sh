db85895a8160cbd4dc23f72ebf88c7d0038395ec
Bowo <wibowosulistio@gmail.com>
2017-05-09 21:14:28 +0700

    Update bootstrap.sh

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -12,6 +12,7 @@ echo "Setting up your EasyEngine-Ubuntu-Vagrant box..."
 # EasyEngine
 
 echo "Installing EasyEngine..."
+echo -e "[user]\n\tname = Your Name\n\temail = your@email.com" > ~/.gitconfig
 wget -qO ee rt.cx/ee && sudo bash ee
 
 echo "Installing EasyEngine stack..."
@@ -27,7 +28,7 @@ end_seconds="$(date +%s)"
 echo "+---------------------------------------------------------+"
 echo "|                      S U C C E S S                      |"
 echo "|                Provisioning completed in                |"
-echo "|                   "$(( end_seconds - start_seconds ))" seconds                |"
+echo "|                       "$(( end_seconds - start_seconds ))" seconds                |"
 echo "+---------------------------------------------------------+"
 echo "|   You're good to go! You can now view your server at    |"
 echo "|      \"192.168.50.4/\" or \"ee.dev\" in a browser.      |"
