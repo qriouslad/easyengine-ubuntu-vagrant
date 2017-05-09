01451c54e1f8896cf64a14255649ef6b3fa30226
Bowo <wibowosulistio@gmail.com>
2017-05-09 15:44:07 +0700

    Create bootstrap.sh with base script

---
+++ .provision/bootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "(Setting up your Vagrant box...)"
+
+echo "(Updating apt-get...)"
+sudo apt-get update > /dev/null 2>&1
+
+# EasyEngine
+echo "(Installing EasyEngine...)"
+wget -qO ee rt.cx/ee && sudo bash ee
+
+echo "(Setting Ubuntu (user) password to \"easyengine\"...)"
+echo "ubuntu:easyengine" | chpasswd
+
+echo "+---------------------------------------------------------+"
+echo "|                      S U C C E S S                      |"
+echo "+---------------------------------------------------------+"
+echo "|   You're good to go! You can now view your server at    |"
+echo "|                 \"5.5.5.5/\" in a browser.              |"
+echo "|                                                         |"
+echo "|  If you haven't already, I would suggest editing your   |"
+echo "|     hosts file with \"5.5.5.5  easyengine.dev\"    |"
+echo "|         so that you can view your server with           |"
+echo "|      \"easyengine.dev/\" instead of \"5.5.5.5/\"     |"
+echo "|                      in a browser.                      |"
+echo "|                                                         |"
+echo "|          You can SSH in with ubuntu / vagrant           |"
+echo "|                                                         |"
+echo "|        You can login to MySQL with root / root          |"
+echo "+---------------------------------------------------------+"
