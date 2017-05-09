f0d8c3b15b5797fbfae5359caa205e3637c472b5
Bowo <wibowosulistio@gmail.com>
2017-05-09 20:32:41 +0700

    Create index.php

---
+++ www/22222/htdocs/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EasyEngine Dashboard</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.0/css/foundation.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="top-bar">
+    <div class="top-bar-left">
+        <ul class="dropdown menu" data-dropdown-menu>
+            <li class="menu-text">EasyEngine Dashboard</li>
+            <li><a href="/php/info.php">phpinfo();</a></li>
+            <li><a href="/db/pma">phpMyAdmin</a></li>
+        </ul>
+    </div>
+
+    <div class="top-bar-right">
+        <ul class="dropdown menu" data-dropdown-menu>
+            <li><a href="https://github.com/qriouslad/easyengine-ubuntu-vagrant/">easyengine-ubuntu-vagrant</a></li>
+        </ul>
+    </div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.0/js/foundation.min.js"></script>
+<script>$( document ).foundation();</script>
+
+</body>
+</html>
