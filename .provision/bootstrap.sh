45bdbb5b9355b463ff62abbbe57606f5f439a92c
Bowo <wibowosulistio@gmail.com>
2017-05-09 20:44:00 +0700

    Update bootstrap.sh

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
-echo "Setting up your Vagrant box..."
+# By storing the date now, we can calculate the duration of provisioning at the
+# end of this script.
+start_seconds="$(date +%s)"
 
-echo "Updating apt-get..."
-sudo apt-get update
+echo "Setting up your EasyEngine-Ubuntu-Vagrant box..."
+
+# echo "Updating apt-get..."
+# sudo apt-get update
 
 # EasyEngine
+
 echo "Installing EasyEngine..."
 wget -qO ee rt.cx/ee && sudo bash ee
+
+echo "Installing EasyEngine stack..."
+sudo ee stack install
 
 echo "Setting Ubuntu (user) password to \"easyengine\"..."
 echo "ubuntu:easyengine" | chpasswd
 
+#set +xv
+# And it's done
+end_seconds="$(date +%s)"
+
 echo "+---------------------------------------------------------+"
 echo "|                      S U C C E S S                      |"
+echo "|                Provisioning completed in                |"
+echo "|                   "$(( end_seconds - start_seconds ))" seconds                |"
 echo "+---------------------------------------------------------+"
 echo "|   You're good to go! You can now view your server at    |"
-echo "|                 \"127.0.0.1/\" in a browser.            |"
-echo "|                                                         |"
-echo "|  If you haven't already, I would suggest editing your   |"
-echo "|     hosts file with \"127.0.0.1  easyengine.dev\"       |"
-echo "|         so that you can view your server with           |"
-echo "|      \"easyengine.dev/\" instead of \"127.0.0.1/\"      |"
-echo "|                      in a browser.                      |"
+echo "|      \"192.168.50.4/\" or \"ee.dev\" in a browser.      |"
 echo "|                                                         |"
 echo "|           You can SSH with ubuntu / easyengine          |"
 echo "|                                                         |"
